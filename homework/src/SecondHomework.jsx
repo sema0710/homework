1200f9e942c2311fc5b40b657869a97c090af378
ohjunsang <pandati@naver.com>
2019-08-15 17:25:32 +0900

    숙제 완료
    숙제 완료

--- homework/src/SecondHomework.jsx
+++ homework/src/SecondHomework.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './mainCss.css'
 
 
 
 const StyledCheckBox = ()=>{
-
+    const [ checkBoxvisibility , setCheckBox ] = useState("hidden");
+    const [ CheckBoxToggle , setCheckBoxToggle ] = useState("off")
+    const [ radioVisibility , setRadio] = useState("hidden");
+    const [ radioToggle , setRadioToggle ] = useState("off");
     return(
-        <div id="StyledCheckBoxDiv">
-            <input type="checkbox" id="realCheckBox"/>
-            <label id="fakeCheckBox" htmlFor="realCheckBox">
-                <span id="fakeCheckBoxImg">✔</span>
-            </label>
-            <span id="CheckBoxChecker"></span>
+        <div id="StyledComponents">
+            <div>
+                <div id="StyledCheckBox" onClick={
+                    ()=>{
+                        if(CheckBoxToggle === "off"){
+                            setCheckBox("visible");
+                            setCheckBoxToggle("on")
+                        }
+                        else{
+                            setCheckBox("hidden");
+                            setCheckBoxToggle("off")
+                        }
+                    }
+                }>
+                    <div id="StyledCheckBoxIconDiv">
+                        <span id="StyledCheckBoxIcon" style={{visibility : checkBoxvisibility}}>✔</span>
+                    </div>
+                    <div>
+                        <span>{CheckBoxToggle}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div id="StyledRadioDiv">
+                <div id="StyledRadio" onClick={
+                    ()=>{
+                        if(radioToggle === "off"){
+                            setRadio("visible");
+                            setRadioToggle("on");
+                        }
+                        else{
+                            setRadio("hidden");
+                            setRadioToggle("off");
+                        }
+                    }
+                }>
+                    <div id="StyledRadioIcon" style={{visibility : radioVisibility}}></div>
+                </div>
+                <div>
+                    <span>{radioToggle}</span>
+                </div>
+
+            </div>
         </div>
     )
 }
